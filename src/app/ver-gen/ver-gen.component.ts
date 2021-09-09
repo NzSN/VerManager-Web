e6b2724c5b71a05603a4df635e7b4f650a2c5a04
NzSN <aydenlin@foxmail.com>
2021-09-09 17:25:37 +0800

    Add extra to generate.

--- src/app/ver-gen/ver-gen.component.ts
+++ src/app/ver-gen/ver-gen.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { VersionService } from '../version.service';
-import { RevisionService } from '../revision.service';
 import { Version, VersionBuild, BuildInfo } from '../version';
-import { Revision } from '../revision';
+
+
+interface ExtraDict {
+    [index: string]: string;
+}
 
 
 @Component({
@@ -13,35 +16,31 @@ import { Revision } from '../revision';
 export class VerGenComponent implements OnInit {
 
     versions: Version[] = [];
-    revisions: Revision[] = [];
 
     constructor(
         private verService: VersionService,
-        private revService: RevisionService
     ) { }
 
     ngOnInit(): void {
-        this.refresh()
+        this.refresh();
     }
 
-    generate(version: Version | undefined, ...infos: string[]): void {
-        let buildInfo: BuildInfo = {};
+    generate(version: Version | undefined, extraOpt: string): void {
+        if (typeof version == 'undefined')
+            return;
 
-        if (typeof version !== 'undefined') {
+        let extra_info: ExtraDict = {};
 
-            if (infos.length === 2) {
-                buildInfo = { logFrom: infos[0], logTo: infos[1] };
-            }
-
-            const build: VersionBuild = { ver: version, info: buildInfo };
-            this.verService.generate(build).subscribe();
+        if (extraOpt !== "") {
+            extra_info['extra'] = extraOpt;
         }
+
+        const build: VersionBuild = { ver: version, info: extra_info };
+        this.verService.generate(build).subscribe();
     }
 
     refresh(): void {
         this.verService.getVersions()
             .subscribe(versions => this.versions = versions);
-        this.revService.getRevisions()
-            .subscribe(revisions => this.revisions = revisions);
     }
 }
